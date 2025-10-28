30e5fe55c1885819298ace983320097cca638b98
Vijay Kumar <iamvijaykumar51@gmail.com>
2025-10-28 22:01:35 +0530

    Add files via upload

---
+++ 18. Windows Function.sql
@@ -0,0 +1,80 @@
+use regex;
+CREATE TABLE EmployeeSales (
+    EmployeeID INT,
+    EmployeeName VARCHAR(100),
+    Department VARCHAR(50),
+    SaleDate DATE,
+    SaleAmount DECIMAL(10, 2)
+);
+
+
+INSERT INTO EmployeeSales (EmployeeID, EmployeeName, Department, SaleDate, SaleAmount) VALUES
+(1, 'Alice', 'Sales', '2025-06-01', 1200.00),
+(1, 'Alice', 'Sales', '2025-06-03', 1800.00),
+(1, 'Alice', 'Sales', '2025-06-07', 1500.00),
+(2, 'Bob', 'Sales', '2025-06-01', 2000.00),
+(2, 'Bob', 'Sales', '2025-06-05', 2200.00),
+(2, 'Bob', 'Sales', '2025-06-08', 2100.00),
+(3, 'Carol', 'Marketing', '2025-06-02', 3000.00),
+(3, 'Carol', 'Marketing', '2025-06-06', 2800.00),
+(3, 'Carol', 'Marketing', '2025-06-10', 2700.00),
+(4, 'Dave', 'Marketing', '2025-06-01', 1000.00),
+(4, 'Dave', 'Marketing', '2025-06-03', 1100.00),
+(4, 'Dave', 'Marketing', '2025-06-09', 1300.00),
+(5, 'Eve', 'HR', '2025-06-04', 900.00),
+(5, 'Eve', 'HR', '2025-06-07', 950.00),
+(5, 'Eve', 'HR', '2025-06-10', 1000.00),
+(6, 'Frank', 'Sales', '2025-06-02', 1700.00),
+(6, 'Frank', 'Sales', '2025-06-05', 1900.00),
+(6, 'Frank', 'Sales', '2025-06-09', 1600.00),
+(7, 'Grace', 'Finance', '2025-06-01', 2500.00),
+(7, 'Grace', 'Finance', '2025-06-06', 2400.00),
+(7, 'Grace', 'Finance', '2025-06-10', 2600.00),
+(8, 'Hank', 'Finance', '2025-06-03', 2300.00),
+(8, 'Hank', 'Finance', '2025-06-04', 2200.00),
+(8, 'Hank', 'Finance', '2025-06-08', 2100.00),
+(9, 'Ivy', 'HR', '2025-06-02', 800.00),
+(9, 'Ivy', 'HR', '2025-06-06', 850.00),
+(9, 'Ivy', 'HR', '2025-06-09', 950.00),
+(10, 'Jake', 'IT', '2025-06-05', 3000.00),
+(10, 'Jake', 'IT', '2025-06-07', 3200.00),
+(10, 'Jake', 'IT', '2025-06-10', 3100.00);
+
+select * from employeeSales;
+
+select sum(saleamount) FROM employeeSales;
+
+select *, (select sum(saleamount) from employeesales ) from employeesales;
+
+select EmployeeName, sum(saleamount) from employeesales;
+
+
+-- Q. what will be the total amount of each employee
+
+select employeename, sum(saleamount) from employeesales group by employeename;
+
+-- 56750.00
+
+select *, 
+sum(saleAmount) over(),
+avg(saleAmount) over() from employeeSales;
+
+select employeename, saleamount, max(saleamount) over(), min(saleamount) over() from employeesales;
+
+select *, sum(saleamount) over(partition by employeename) from employeesales;
+
+select *, max(saleamount) over(partition by department) from employeesales;
+
+-- running sum (cummulative sum)
+select *,
+sum(saleamount) over(order by saledate desc) from employeesales;
+
+select *,
+avg(saleamount) over(order by saleamount) from employeesales;
+
+-- Department wise running sum
+select *,
+sum(saleamount) over(partition by department order by saleamount) from employeesales;
+
+
+
